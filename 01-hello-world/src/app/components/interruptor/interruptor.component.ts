f762969fbec1a9d038c2a11c4dd3d21d75feaefb
Alfonso Tienda <afoone@hotmail.com>
2022-01-25 12:53:17 +0100

    finished hello world

--- 01-hello-world/src/app/components/interruptor/interruptor.component.ts
+++ 01-hello-world/src/app/components/interruptor/interruptor.component.ts
@@ -3,17 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-interruptor',
   templateUrl: './interruptor.component.html',
-  styleUrls: ['./interruptor.component.css']
+  styleUrls: ['./interruptor.component.css'],
 })
 export class InterruptorComponent implements OnInit {
-
-  public luz: string
+  public luz: string;
 
   constructor() {
-    this.luz= "on"
-   }
-
-  ngOnInit(): void {
+    this.luz = 'off';
   }
 
+  toggle() {
+    this.luz = this.luz === 'off' ? 'on' : 'off';
+  }
+
+  ngOnInit(): void {}
 }
